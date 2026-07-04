44f2a4f365488c2d3a6d51b9d1ae0250b9f0aaf9
Moltas <moltas.jonsson@icloud.com>
2026-07-04 17:47:55 +0200

    Initial Commit

---
+++ QuickNotepad/QuickNotepadApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickNotepadApp.swift
+//  QuickNotepad
+//
+//  Created by Moltas Jönsson on 2026-07-04.
+//
+
+import SwiftUI
+
+@main
+struct QuickNotepadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
